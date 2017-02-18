250b869f5771b7662824d774fcaa978e444f400a
mondrake <mondrake.org>
2017-02-18 14:45:13 +0100

    20170218

--- src/mm/MM_settings.php
+++ src/mm/MM_settings.php
@@ -9,6 +9,7 @@ define('DB_QUERY_PERFORMANCE_THRESHOLD', 100);
 define('IP_LOCATION_SERVICE_ENABLED', true);
 define('IP_LOCATION_SERVICE_KEY', '8628aa053a9cccabd338ff7089b853362c518b5a8182a0a73af01e9879fc4866');
 define('MM_CLASS_PATH', '\\mondrakeNG\\mm\\classes\\');
+define('TEXTIMAGE_URL', "http://mondrake.org/lab02/sites/default/files/textimage/verthead");
 
 $dsn = [
   'driver' => 'pdo_mysql',
