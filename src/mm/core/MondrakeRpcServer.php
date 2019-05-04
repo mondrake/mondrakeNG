12e3ff3b061d2c5dbe1cb446e7c055771b219103
mondrake <mondrake.org@gmail.com>
2019-05-04 10:48:57 +0200

    Update MondrakeRpcServer.php

--- src/mm/core/MondrakeRpcServer.php
+++ src/mm/core/MondrakeRpcServer.php
@@ -297,8 +297,8 @@ throw new \exception('test');*/
     public static function uploadDocs($xmlrpcmsg) {
       $encoder = new Encoder();
       $n = $encoder->decode($xmlrpcmsg);    
+error_log(var_export($n, true));
       $arr = $n[0];
-error_log(var_export($arr, true));
       $srvRunTime = new MMTimer;
       $srvRunTime->start();
       $diag = new MMDiag;
