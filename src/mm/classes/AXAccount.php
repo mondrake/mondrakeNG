604675c75b717c1ce552627d10e16df11cd76d33
mondrake <mondrake.org@gmail.com>
2019-09-01 14:04:23 +0200

    Update AXAccount.php

--- src/mm/classes/AXAccount.php
+++ src/mm/classes/AXAccount.php
@@ -183,6 +183,7 @@ class AXAccount extends MMObj
         echo '<table class="signup" border="1" cellpadding="2" cellspacing="0" bgcolor="#eeeeee">';
         $trav = new RbppavlTraverser($tree);
         $el = $trav->first();
+        $thSet = false;
         while ($el) {
             if (!$thSet) {
                 echo "<tr>";
